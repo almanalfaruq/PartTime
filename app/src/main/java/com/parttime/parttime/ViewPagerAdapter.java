f9ab1c6ef0cc274eff66e74a6f2202ff18ab95b7
Unknown <danasetyaji@gmail.com>
2018-04-19 12:40:05 +0700

    Menambah Main Activity dan Navbar

---
+++ app/src/main/java/com/parttime/parttime/ViewPagerAdapter.java
@@ -0,0 +1,26 @@
+package com.parttime.parttime;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by danasw on 19/04/2018.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
